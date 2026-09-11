41710db1dc7713096ebd42c0bcd98d9dff4e5158
viooo <benedicvio@gmail.com>
2026-09-11 10:39:29 +0700

    feat: pasang aset favicon jamur dan cache buster untuk server backend produksi

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -4,7 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Smart Shroom SCM') }}</title>
+
+        <link rel="icon" type="image/png" href="/favicon.png?v=2" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        <link rel="apple-touch-icon" href="/favicon.png?v=2" />
 
         @fonts
 
